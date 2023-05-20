68c4017598ec9db8e788d1b9e3a9e94a7089a39b
juanjopuro <juanjopuro18@gmail.com>
2023-05-20 08:41:37 -0500

    Servicio componentes

--- src/Servicios/Servicios.jsx
+++ src/Servicios/Servicios.jsx
@@ -18,11 +18,9 @@ export function Servicios() {
                 alt="..."
               />
               <div className="card-body">
-                <h5 className="card-title">Servicio 1</h5>
+                <h5 className="card-title">Cuidado personal</h5>
                 <p className="card-text">
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
+                Brindar asistencia en la higiene personal diaria, como bañarse, vestirse, afeitarse, peinarse y cuidado dental, para garantizar el bienestar y la comodidad de los adultos mayores.
                 </p>
               </div>
             </div>
@@ -35,11 +33,10 @@ export function Servicios() {
                 alt="..."
               />
               <div className="card-body">
-                <h5 className="card-title">Servicio 2</h5>
+                <h5 className="card-title">Administración de medicamentos</h5>
                 <p className="card-text">
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
+                Ayudar a los adultos mayores a tomar sus medicamentos de manera adecuada y en los momentos correctos, siguiendo las indicaciones médicas y asegurando la adherencia al tratamiento.
+
                 </p>
               </div>
             </div>
@@ -52,10 +49,119 @@ export function Servicios() {
                 alt="..."
               />
               <div className="card-body">
-                <h5 className="card-title">Servicio 3</h5>
+                <h5 className="card-title">Acompañamiento y actividades recreativas</h5>
                 <p className="card-text">
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content.
+                Proporcionar compañía a los adultos mayores, participar en actividades recreativas y fomentar la interacción social para evitar el aislamiento y promover un estilo de vida activo y saludable.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-4">
+            <div className="card">
+              <img
+                src="https://firebasestorage.googleapis.com/v0/b/retreatsanctuary-379f9.appspot.com/o/servicio3.webp?alt=media&token=4ffd5fae-ae40-4516-ad57-adeed211608c"
+                className="card-img-top"
+                alt="..."
+              />
+              <div className="card-body">
+                <h5 className="card-title">Cuidado de la alimentación</h5>
+                <p className="card-text">
+                Preparar comidas nutritivas y adaptadas a las necesidades dietéticas de los adultos mayores, supervisar la ingesta de alimentos y proporcionar asistencia durante las comidas si es necesario.
+              
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-4">
+            <div className="card">
+              <img
+                src="https://firebasestorage.googleapis.com/v0/b/retreatsanctuary-379f9.appspot.com/o/servicio3.webp?alt=media&token=4ffd5fae-ae40-4516-ad57-adeed211608c"
+                className="card-img-top"
+                alt="..."
+              />
+              <div className="card-body">
+                <h5 className="card-title">Gestión de la medicación</h5>
+                <p className="card-text">
+                Coordinar y administrar los medicamentos prescritos por los profesionales de la salud, asegurando que se soliciten y reabastezcan los medicamentos a tiempo, y manteniendo un registro preciso de la medicación
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-4">
+            <div className="card">
+              <img
+                src="https://firebasestorage.googleapis.com/v0/b/retreatsanctuary-379f9.appspot.com/o/servicio3.webp?alt=media&token=4ffd5fae-ae40-4516-ad57-adeed211608c"
+                className="card-img-top"
+                alt="..."
+              />
+              <div className="card-body">
+                <h5 className="card-title">Servicios de transporte</h5>
+                <p className="card-text">
+                Ayudar a los adultos mayores a desplazarse hacia y desde citas médicas, supermercados, eventos sociales u otras actividades, asegurando su seguridad y comodidad durante el viaje.
+
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-4">
+            <div className="card">
+              <img
+                src="https://firebasestorage.googleapis.com/v0/b/retreatsanctuary-379f9.appspot.com/o/servicio3.webp?alt=media&token=4ffd5fae-ae40-4516-ad57-adeed211608c"
+                className="card-img-top"
+                alt="..."
+              />
+              <div className="card-body">
+                <h5 className="card-title">Asistencia en la movilidad</h5>
+                <p className="card-text">
+                Proporcionar apoyo físico y asistencia en la movilidad a los adultos mayores, utilizando dispositivos de ayuda: como andadores o bastones y ayudándolos a realizar ejercicios de movilidad para mantener la fuerza y la flexibilidad.
+
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-4">
+            <div className="card">
+              <img
+                src="https://firebasestorage.googleapis.com/v0/b/retreatsanctuary-379f9.appspot.com/o/servicio3.webp?alt=media&token=4ffd5fae-ae40-4516-ad57-adeed211608c"
+                className="card-img-top"
+                alt="..."
+              />
+              <div className="card-body">
+                <h5 className="card-title">Servicios de terapia ocupacional</h5>
+                <p className="card-text">
+                Colaborar con terapeutas ocupacionales para implementar programas que ayuden a los adultos mayores a mantener o mejorar su capacidad funcional en actividades cotidianas, como vestirse, cocinar o realizar tareas domésticas.
+
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-4">
+            <div className="card">
+              <img
+                src="https://firebasestorage.googleapis.com/v0/b/retreatsanctuary-379f9.appspot.com/o/servicio3.webp?alt=media&token=4ffd5fae-ae40-4516-ad57-adeed211608c"
+                className="card-img-top"
+                alt="..."
+              />
+              <div className="card-body">
+                <h5 className="card-title">Servicios de enfermería</h5>
+                <p className="card-text">
+                Contar con personal de enfermería capacitado para realizar chequeos de salud periódicos, monitorear signos vitales, administrar inyecciones y proporcionar atención médica básica, según las necesidades individuales de cada adulto mayor.
+
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-4">
+            <div className="card">
+              <img
+                src="https://firebasestorage.googleapis.com/v0/b/retreatsanctuary-379f9.appspot.com/o/servicio3.webp?alt=media&token=4ffd5fae-ae40-4516-ad57-adeed211608c"
+                className="card-img-top"
+                alt="..."
+              />
+              <div className="card-body">
+                <h5 className="card-title">Asesoramiento y apoyo emocional</h5>
+                <p className="card-text">
+                Brindar orientación y apoyo emocional a los adultos mayores y sus familias, ayudándolos a lidiar con los desafíos emocionales y psicológicos asociados con el envejecimiento, y facilitando la conexión con recursos comunitarios adicionales según sea necesario.
                 </p>
               </div>
             </div>
